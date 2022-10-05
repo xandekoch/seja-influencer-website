ea476b445976f3c6cbc636153e630faa4d259557
Alexandre Koch <85593242+xandekoch@users.noreply.github.com>
2022-10-05 08:11:53 -0300

    add carousels and upgrades

---
+++ main.js
@@ -0,0 +1,46 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: "auto",
+    spaceBetween: 20,
+    // slidesPerGroup: 1,
+    freeMode: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+
+var swiper = new Swiper(".mySwiper2", {
+    slidesPerView: "auto",
+    spaceBetween: 15,
+    loop: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    freeMode: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
+
+var swiper = new Swiper(".mySwiper3", {
+    slidesPerView: "auto",
+    spaceBetween: 20,
+    // slidesPerGroup: 1,
+    freeMode: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
