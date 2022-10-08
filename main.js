d5d54e2cc956ff36407590fc8efe3d6b6398ab54
Alexandre Koch <85593242+xandekoch@users.noreply.github.com>
2022-10-08 01:57:44 -0300

    fix bugs and finish sections

--- main.js
+++ main.js
@@ -44,3 +44,72 @@ var swiper = new Swiper(".mySwiper3", {
         prevEl: ".swiper-button-prev",
     },
 });
+
+
+function openLightbox() {
+    document.body.classList.add("video");
+}
+
+
+function closeLightbox() {
+    document.body.classList.remove("video");
+}
+
+
+function playVideo() {
+    console.log('playVideo')
+    let video = document.getElementById("video")
+    video.contentWindow.postMessage( '{"event":"command", "func":"playVideo", "args":""}', '*');
+}
+
+
+function pauseVideo() {
+    console.log('pauseVideo')
+    let video = document.getElementById("video")
+    video.contentWindow.postMessage( '{"event":"command", "func":"pauseVideo", "args":""}', '*');
+}
+
+
+function openCard(card) {
+    classes = card.classList
+    console.log(classes)
+    if ('opened' == card.classList[1]) {
+    card.classList.remove("opened");
+    card.classList.add("closed")
+    }
+    else {
+        card.classList.remove("closed");
+        card.classList.add("opened")
+    }
+}
+
+
+// Set the date we're counting down to
+var countDownDate = new Date("Oct 7, 2022 23:55:00").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+  document.getElementById("hour").innerHTML = hours;
+  document.getElementById("minute").innerHTML = minutes;
+  document.getElementById("second").innerHTML = seconds;
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("hour").innerHTML = "EXPIRED";
+  }
+}, 1000);
